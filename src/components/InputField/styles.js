9cad91851182ae78ff6284296b403f056d1bba35
Apple retina <appleretina@Apples-MacBook-Pro-6.local>
2021-12-03 21:41:44 +0530

    LoginPage

--- src/components/InputField/styles.js
+++ src/components/InputField/styles.js
@@ -3,7 +3,7 @@ import { mobile } from "../../responsive";
 export const Input = styled.input`
   flex: 1;
   min-width: 40%;
-  margin: 20px 10px 0px 0px;
+  margin: 10px 10px 10px 0px;
   padding: 10px;
   border: 1px solid grey;
   border-radius: 4%;
